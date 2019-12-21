22d5ddf21951f388ca5a7916ed4c6654045bbfad
Inna Nikolenko <innaivasyuk93@gmail.com>
2019-12-20 23:32:52 -0800

    Created seeding scripts

---
+++ scripts/query-rooms.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-console */
+const { Client } = require('pg');
+const fs = require('fs');
+// const data = require('../data/rooms.json');
+
+const client = new Client({
+  user: 'inna',
+  host: 'localhost',
+  password: 'password',
+  database: 'booking',
+  port: 5432,
+});
+// console.log('data is ', data);
+
+client.connect(() => {
+  console.log('hi I connected');
+
+  function saveToDb(room) {
+    return new Promise((resolve, reject) => {
+      console.log('saving...')
+      const text = 'INSERT INTO rooms (roomname, price, cleaning_fee, service_fee, tax, max_guest, min_night, max_night, ratings, num_reviews) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
+      const values = [room.roomname, room.price, room.cleaning_fee, room.service_fee, room.tax, room.max_guest, room.min_night, room.max_night, room.ratings, room.num_reviews];
+      client.query(text, values).then(() => {
+        // console.log('saved to db');
+        resolve();
+      }).catch((err) => {
+        console.log('err is ', err);
+        reject();
+      });
+    });
+  }
+
+  // const promises = [];
+
+  fs.readdir('../data', (err, items) => {
+    // console.log(items);
+    let count = 0;
+    // for (let i = 0; i < items.length; i += 1) {
+    setInterval(() => {
+      console.log(items[count]);
+      const promises = [];
+      const rawData = fs.readFileSync(`../data/${items[count]}`);
+      const data = JSON.parse(rawData);
+      for (let j = 0; j < data.length; j += 1) {
+        promises.push(saveToDb(data[j]));
+      }
+
+      Promise.all(promises)
+        .then(() => {
+          console.log('saved');
+          count += 1;
+          // client.end();
+        }).catch(() => {
+          console.log('something went wrong');
+          process.exit();
+        });
+    }, 30000);
+
+      // fs.readFile(items[i], (error, data) => {Í
+      //   for (let j = 0; j < data.length; j += 1) {
+      //     promises.push(saveToDb(data[i]));
+      //   }
+      // });
+    // }
+  });
+
+  // for (let i = 0; i < data.length; i += 1) {
+  //   promises.push(saveToDb(data[i]));
+  // }
+
+  // Promise.all(promises)
+  //   .then(() => {
+  //     console.log('saved');
+  //     client.end();
+  //   }).catch(() => {
+  //     console.log('something went wrong');
+  //   });
+});
---
+++ scripts/seed-rooms.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const faker = require('faker');
+
+const roomNameAppendix = ['\'s Apartment', '\'s House', '\'s Loft', '\'s Condo'];
+
+// roomname:
+// price : 50 ~ 200
+// cleaning fee: 5
+// service fee: 5
+// tax: 10
+// max_guest: {adults: 5, children: 3, infants: 2}
+//     random between 1-6 for adult, random between 0-4 kids
+// min_night: 1
+// max_night: random between 2-6
+// ratings: random 0.0-5.0
+// num_reviews: random 0-100
+
+// min and max included
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Random Rooms
+function generateRandomRooms(num) {
+  const rooms = [];
+
+  for (let i = 0; i < num; i += 1) {
+
+    const room = {
+      roomname: faker.name.findName()
+        + roomNameAppendix[randomIntFromInterval(0, roomNameAppendix.length - 1)],
+      price: randomIntFromInterval(50, 200),
+      cleaning_fee: 5,
+      service_fee: 5,
+      tax: 10,
+      max_guest: {
+        adults: randomIntFromInterval(1, 6),
+        children: randomIntFromInterval(0, 4),
+        infants: randomIntFromInterval(0, 2),
+      },
+      min_night: randomIntFromInterval(1, 2),
+      max_night: randomIntFromInterval(3, 6),
+      ratings: (Math.random() * (5.0 - 1.0) + 1.0).toFixed(1),
+      num_reviews: randomIntFromInterval(0, 100),
+    };
+    rooms.push(room);
+  }
+
+  return rooms;
+}
+
+for (let j = 0; j < 500; j += 1) {
+  const result = JSON.stringify(generateRandomRooms(20000));
+
+  fs.writeFileSync(`../data/rooms${j}.json`, result);
+}
+
+// function fileWriteSync(filepath) {
+//   const fd = fs.openSync(filepath, 'w');
+//   for (let i = 0; i < 20 ; i += 1) {
+//     let data = JSON.stringify(generateRandomRooms(500000));
+//     fs.writeSync(fd, data);
+//   }
+
+//   fs.closeSync(fd);
+// }
+
+// fileWriteSync('../data/rooms.json');
+// fs.writeFile(`../data/rooms${j}.json`, result, (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Data saved!');
+//   }
+// });
